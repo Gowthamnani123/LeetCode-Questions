724861f5e18deb09ff12dbd50e933fd385f849d8
Kalamandalapadu Gowtham <87270055+Gowthamnani123@users.noreply.github.com>
2022-04-29 10:49:48 +0530

    Time: 26 ms (56.91%), Space: 7.1 MB (92.62%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   int change(int amt, vector<int>& a) {
+        int n=a.size();
+        int i,j;
+        
+        vector<int> dp(amt+1,0);
+        dp[0]=1;
+        
+        for(i=0;i<n;i++)
+        {
+            for(j=1;j<=amt;j++)
+            {
+                if(a[i]<=j)
+                    dp[j]+=dp[j-a[i]];
+            }
+        }    
+        return dp[amt];
+    }   
+};
