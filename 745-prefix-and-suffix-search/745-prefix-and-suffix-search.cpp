714b89c1bd8e0bcb6271ae04c2ef0af1030bc3a0
Kalamandalapadu Gowtham <87270055+Gowthamnani123@users.noreply.github.com>
2022-06-18 11:15:51 +0530

    Time: 563 ms (70.90%), Space: 187.5 MB (37.69%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -0,0 +1,106 @@
+ class Trie
+{
+    public:
+    struct Node
+    {
+        private:
+            Node *child[27];
+            int endIdx;
+        
+        public:
+            bool contains(char &ch)
+            {                    
+                return (child[ch - 'a'] != NULL);
+            } 
+        
+            void putNode(char &ch, Node *newNode)
+            {
+                child[ch - 'a'] = newNode;
+            }
+        
+            Node *getNext(char &ch)
+            {                
+                return child[ch - 'a'];
+            }
+            
+            void setIdx(int &i)
+            {
+                endIdx = i;
+            }
+        
+            int getIdx()
+            {
+                return endIdx;
+            }
+    };
+    
+    Node *root;
+    
+    public:
+    Trie()
+    {
+        root = new Node();
+    }
+    
+    void insert(string &word, int &idx)
+    {
+        Node *temp = root;
+        
+        for(auto &ch : word)
+        {
+            if(!temp -> contains(ch))
+            {
+                temp -> putNode(ch, new Node());
+            }
+            
+            temp = temp -> getNext(ch);
+            temp -> setIdx(idx);
+        }
+        
+    }
+    
+    int startsWith(string &word)
+    {
+        Node *temp = root;
+        
+        for(auto &ch : word)
+        {
+            if(!temp -> contains(ch))
+            {
+                return -1;
+            }
+            
+            temp = temp -> getNext(ch);
+        }
+        
+        return temp -> getIdx();
+    }
+};
+
+class WordFilter {
+    Trie trie;
+public:
+    WordFilter(vector<string>& words) {
+        int len = words.size();
+        for(int i = 0; i < len; i++)
+        {            
+            string str = words[i];
+            int n = str.size();
+            
+            for(int j = 0; j <= n; j++)
+            {                
+                string cur = str.substr(j) + '{' + str;
+                
+                trie.insert(cur, i);
+            }
+        }
+    }
+    
+    int f(string prefix, string suffix) {
+        
+        string cur = suffix + '{' + prefix;
+        
+        return trie.startsWith(cur);
+    }
+};
+
