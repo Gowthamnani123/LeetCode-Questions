abebc684850ade839954b1a6dbf486a5b752829b
Kalamandalapadu Gowtham <87270055+Gowthamnani123@users.noreply.github.com>
2022-08-25 22:14:42 +0530

    Time: 21 ms (65.89%), Space: 8.8 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+     bool canConstruct(string r, string m) {
+        int f[26]={0};
+        for(char c:m)
+            f[c-'a']++;
+        for(char c:r)
+        {
+            if(--f[c-'a']<0)
+                return 0;
+        }  
+        return 1;
+    }
+};
