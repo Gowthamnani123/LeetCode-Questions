fd03c1a4a48a68570b330f9808f989f198364502
Kalamandalapadu Gowtham <87270055+Gowthamnani123@users.noreply.github.com>
2022-09-23 16:05:36 +0530

    Time: 4 ms (67.01%), Space: 7.8 MB (55.18%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+   vector<vector<int>> ans;
+    vector<int> t;
+    
+    void permute(vector<int>& a,int in,int n)
+    {
+        if(in==n)
+        {
+            ans.push_back(t);
+            return;
+        }
+        
+        for(int i=in;i<n;i++)
+        {
+            swap(a[i],a[in]);
+            t.push_back(a[in]);
+            
+            permute(a,in+1,n);
+            
+            t.pop_back();
+            swap(a[i],a[in]);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& a) {
+        permute(a,0,a.size());
+        return ans;
+    }
+};
