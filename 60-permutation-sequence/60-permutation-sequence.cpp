1c7fcaf7a96b7f34ce979912d524f349a2985dde
Kalamandalapadu Gowtham <87270055+Gowthamnani123@users.noreply.github.com>
2022-09-23 19:09:33 +0530

    Time: 6 ms (34.56%), Space: 6 MB (81.78%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+      string ans;
+    
+    void permute(int n, int k,int *fact,vector<int> &a)
+    {
+        if(n==1)
+        {
+            ans+=(a[0]+'0');
+            return ;
+        }
+        
+        int blocksize=fact[n-1];
+        int in=k/blocksize;
+        
+        ans+=(a[in]+'0');
+        a.erase(a.begin()+in);
+        k=k%blocksize;
+        
+        permute(n-1,k,fact,a);
+    }
+    
+    string getPermutation(int n, int k) {
+        int fact[n+1];
+        int i;
+        vector<int> a(n);
+        
+        fact[0]=1;
+        for(i=1;i<=n;i++)
+        {
+            fact[i]=fact[i-1]*i;
+            a[i-1]=i;
+        }
+            
+        ans="";
+        permute(n,k-1,fact,a);
+        
+        return ans;
+    }
+};
